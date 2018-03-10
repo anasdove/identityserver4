f58bd2ea97ef20bb0698dee7b402372558d46ff4
Choirul Anas <choirulanas2011@gmail.com>
2018-03-10 19:00:12 +0700

    Create IdentityController to test the authorization and setup the startup.cs

---
+++ src/CADove.WebApi/Constant.cs
@@ -0,0 +1,19 @@
+﻿namespace CADove.WebApi
+{
+    public static class Constant
+    {
+        public const string BASE_URI = "http://localhost:5005";
+
+        public struct ApiResource
+        {
+            public const string NAME = "CADoveApi";
+            public const string DISPLAY = "CA Dove API";
+        }
+
+        public struct Client
+        {
+            public const string CLIENT_ID = "CADoveClientId";
+            public const string SECRET = "secret";
+        }
+    }
+}
--- src/CADove.WebApi/Startup.cs
+++ src/CADove.WebApi/Startup.cs
@@ -1,13 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using IdentityServer4.AccessTokenValidation;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
 
 namespace CADove.WebApi
 {
@@ -23,17 +19,25 @@ namespace CADove.WebApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
+            services.AddMvcCore()
+                .AddAuthorization()
+                .AddJsonFormatters();
+
+            services.AddAuthentication(IdentityServerAuthenticationDefaults.AuthenticationScheme)
+                .AddIdentityServerAuthentication(options =>
+                {
+                    options.Authority = Constant.BASE_URI;
+                    options.RequireHttpsMetadata = false; // Set as FALSE for development only.
+                    options.ApiName = Constant.ApiResource.NAME;
+                });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
+            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            loggerFactory.AddDebug();
+            app.UseAuthentication();
             app.UseMvc();
         }
     }
